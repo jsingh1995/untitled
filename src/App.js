3fb48a11cbae5b6283a112f79bbe1071c0d41134
jsingh1995 <46855377+jsingh1995@users.noreply.github.com>
2021-04-19 19:33:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
-import UploadImage from './components/UploadImage';
+//import UploadImage from './components/UploadImage';
 
 export default function App() {
   return (
